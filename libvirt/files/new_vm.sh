ab7cb514ca47c6ed891e1dc979bcba39fcca9bad
Ben Brown <xthor@xthorsworld.com>
2022-05-11 18:33:19 -0600

    working on libvirt state for Rocky Linux and friends

--- libvirt/files/new_vm.sh
+++ libvirt/files/new_vm.sh
@@ -114,78 +114,13 @@ if [ $? -ne 0 ]; then
 	exit 255
 fi
 
-# virt-sysprep to set a few things
-#echo "Updating host image for ${host_name}..."
-# I really wish I didn't have 2 different freaking commands here, but it's not working right otherwise
-#if [ -z "${salted}" ]; then
-#	sudo virt-sysprep -a ${disk_image} --hostname ${host_name} --network --update --selinux-relabel
-#else
-#	sudo virt-sysprep -a ${disk_image} --hostname ${host_name} --network --update --selinux-relabel --run-command "curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh && bash /tmp/install_salt.sh -P -X"
-#fi
-#if [ $? -ne 0 ]; then
-#	echo "Something went wrong with virt-sysprep -- exiting."
-#	exit 255
-#fi
-
-# virt-install older than 3.2 (I think) doesn't support --cloud-init option, which is a bummer
-# TODO: add a check for that!
-#sudo virt-install --virt-type kvm --name ${host_name} --ram ${memory} --vcpus ${vcpus} \
-#	--os-variant ${variant} --network=bridge=${network},model=virtio --graphics vnc \
-#	--disk path=${disk_image},cache=writeback \
-#	--cloud-init root-password-generate=no,disable=on,user-data=/home/xthor/ci/user-data,meta-data=/tmp/meta-data \
-#	--noautoconsole --import
-
-# create a meta-data file
-cat << EOF > /tmp/meta-data
-instance-id: 1
-local-hostname: ${host_name}
-EOF
-
-# create user-data file
-cat << EOF > /tmp/user-data
-#cloud-config
-users:
-    - name: root
-      passwd: resetm3n0w
-      ssh_authorized_keys:
-        - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSUppn5b2njEQSw8FHqyZ0OZiPD14wEejulwnQ7gxLdQYJEqXMleHx4u/9ff3/jDXoGaBFiT2LmUTnpMV8HSj4jsB4PCoFAbq4XnlnwyBx7va/8LQOMdKsjF5W6peO+DYKh+ow9YaJvctzGPebkkNvhI0YFhZod58uoO7lyTnQXkMm8DXl6q7WhNfsZZiwr7tXicUZojU0msMiDpX1JvhGow+mKym0U/6cMgozypYfNbQ2PVkfNnadslp29O5Mfd5X4U+cbACa1sUYYqOT2Zz8C4t5QFXRY1LNokmRbcqbO01bygbE4S2TDnvRz+XZmfZTuw9MMgp7JPfo6cOfDYKf xthor
-    - name: xthor
-      shell: /bin/bash
-      passwd: p@ssw0rd
-      lock_passwd: false
-      sudo: ALL=(ALL) NOPASSWD:ALL
-      ssh_authorized_keys:
-        - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSUppn5b2njEQSw8FHqyZ0OZiPD14wEejulwnQ7gxLdQYJEqXMleHx4u/9ff3/jDXoGaBFiT2LmUTnpMV8HSj4jsB4PCoFAbq4XnlnwyBx7va/8LQOMdKsjF5W6peO+DYKh+ow9YaJvctzGPebkkNvhI0YFhZod58uoO7lyTnQXkMm8DXl6q7WhNfsZZiwr7tXicUZojU0msMiDpX1JvhGow+mKym0U/6cMgozypYfNbQ2PVkfNnadslp29O5Mfd5X4U+cbACa1sUYYqOT2Zz8C4t5QFXRY1LNokmRbcqbO01bygbE4S2TDnvRz+XZmfZTuw9MMgp7JPfo6cOfDYKf xthor
-timezone: America/Denver
-package_upgrade: true
-runcmd:
-    - touch /etc/cloud/cloud-init.disabled
-EOF
-
-# create ci image
-# TODO: salt-ify the user-data file... somehow
-# jesus, cloud-localds is only a Debian thing...
-#sudo cloud-localds -H ${host_name} -d qcow2 ${ci_image} /home/xthor/ci/user-data
-sudo dd if=/dev/zero of=${ci_image} count=1 bs=1M && sudo mkfs.vfat -n cidata ${ci_image}
-if [ $? -eq 0 ]; then
-	# stuff in the user-data and meta-data files
-	sudo mcopy -i ${ci_image} /tmp/meta-data :: && sudo mcopy -i ${ci_image} /tmp/user-data ::
-	if [ $? -ne 0 ]; then
-		echo "Error: could not mcopy user-data or meta-data to ${ci_image}. Exiting."
-		exit 255
-	fi
-else
-	echo "Error: could not create ${ci_image}. Exiting."
-	exit 255
-fi
-
 # kick off virt-install
 echo "Installing VM ${host_name}..."
 sudo virt-install --virt-type kvm --name ${host_name} --ram ${memory} --vcpus ${vcpus} \
 	--os-variant ${variant} --network=bridge=${network},model=virtio --graphics vnc \
 	--disk path=${disk_image},cache=writeback \
 	--disk path=${ci_image} \
+	--sysinfo "system_serial=ds=nocloud-net;h=${host_name};s=http://10.200.54.3/ci/generic/"
 	--noautoconsole --import
 
-rm /tmp/meta-data /tmp/user-data
 exit 0
