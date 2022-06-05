b01f038d19260e5a96054bb276378324ff0df919
Ben Brown <xthor@xthorsworld.com>
2022-06-04 23:26:08 -0600

    adding role for webserver that does cloud-init stuffs

---
+++ ciweb/files/index.php
@@ -0,0 +1,40 @@
+<?php
+if (strpos($_SERVER['REQUEST_URI'],'meta-data') !== false) {
+	$uriExplode = explode('/', $_SERVER['REQUEST_URI']);
+	$vmname = $uriExplode[1];
+	if($vmname == "meta-data") {
+		$vmname = substr(str_shuffle(MD5(microtime())), 0, 10);
+		echo "# no hostname specified in URI, generating random hostname\n";
+	}
+	echo "instance-id: 1\n";
+	echo "local-hostname: " . htmlspecialchars($vmname);
+} elseif(strpos($_SERVER['REQUEST_URI'],'user-data') !== false) {
+	echo '#cloud-config
+users:
+    - name: root
+      plain_text_passwd: resetm3n0w
+      lock_passwd: false
+    - name: xthor
+      shell: /bin/bash
+      plain_text_passwd: resetm3n0w
+      lock_passwd: false
+      sudo: ALL=(ALL) NOPASSWD:ALL
+      ssh_authorized_keys:
+        - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSUppn5b2njEQSw8FHqyZ0OZiPD14wEejulwnQ7gxLdQYJEqXMleHx4u/9ff3/jDXoGaBFiT2LmUTnpMV8HSj4jsB4PCoFAbq4XnlnwyBx7va/8LQOMdKsjF5W6peO+DYKh+ow9YaJvctzGPebkkNvhI0YFhZod58uoO7lyTnQXkMm8DXl6q7WhNfsZZiwr7tXicUZojU0msMiDpX1JvhGow+mKym0U/6cMgozypYfNbQ2PVkfNnadslp29O5Mfd5X4U+cbACa1sUYYqOT2Zz8C4t5QFXRY1LNokmRbcqbO01bygbE4S2TDnvRz+XZmfZTuw9MMgp7JPfo6cOfDYKf xthor
+timezone: America/Denver
+package_upgrade: true
+runcmd:
+    - touch /etc/cloud/cloud-init.disabled
+' ;
+} elseif (strpos($_SERVER['REQUEST_URI'],'vendor-data') !== false) {
+	echo '#vendor-data intentionally left empty';
+} else {
+	echo '
+	<html><head>
+		<title>Metadata Server</title>
+		<h1>Metadata server</h1>
+		<p>you didn\'t say the magic word.</p>
+	</html></head>
+	';
+}
+?>
---
+++ ciweb/init.sls
@@ -0,0 +1,26 @@
+ciweb-packages:
+  pkg.installed:
+    - pkgs:
+      - httpd
+      - php
+      - php-common
+
+httpd:
+  service.running:
+    - enable: True
+
+htaccess_file:
+  file.managed:
+    - name: /var/www/html/.htaccess
+    - source: salt://ciweb/files/htaccess
+    - user: root
+    - group: root
+    - mode: 755
+
+index_php:
+  file.managed:
+    - name: /var/www/html/index.php
+    - source: salt://ciweb/files/index.php
+    - user: root
+    - group: root
+    - mode: 755
