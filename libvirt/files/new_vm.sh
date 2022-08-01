edf5e14c40d3d6c9981490a82815d0d90fbe45a7
Ben Brown <xthor@xthorsworld.com>
2022-08-01 14:46:29 -0600

    added static IP abilities

--- libvirt/files/new_vm.sh
+++ libvirt/files/new_vm.sh
@@ -4,56 +4,59 @@ image_dir=/mnt/cloudimg
 target_dir=/var/lib/libvirt/images
 
 # default options
-flavor="bullseye"
+flavor="rocky8"
 ram=2
 vcpus=1
 storage=10
 
 # display usage
 function usage() {
-	echo "`basename $0`: Deploy a cloud-init templated libvirt VM."
-	echo "Usage:
+  echo "`basename $0`: Deploy a cloud-init templated libvirt VM."
+  echo "Usage:
 
-`basename $0` -h <hostname of VM> -f <flavor> -t <network>"
+`basename $0` -h <hostname of VM> -f <flavor> -t <network> [ -i <ip address> ]
 
-	exit 255
+where <ip address> is in x.x.x.x/xx notation"
+
+  exit 255
 }
 
 function bad_taste() {
-	echo "Error: no flavor named ${flavor} -- exiting."
-	exit 255
+  echo "Error: no flavor named ${flavor} -- exiting."
+  exit 255
 }
 
 # check if tools required by this script are installed
 for x in virt-sysprep mcopy virt-install; do
-	which ${x} >& /dev/null 
-	if [ $? -ne 0 ]; then
-		echo "ERROR: Missing ${x} -- please install."
-		exit 255
-	fi
+  which ${x} >& /dev/null 
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Missing ${x} -- please install."
+    exit 255
+  fi
 done
 
 # get command-line args
-while getopts "h:f:t:s:p:r:m" OPTION; do
-	case $OPTION in
-		h) host_name=${OPTARG};;
-		f) flavor=${OPTARG};;
-		t) network=${OPTARG};;
-		s) storage=${OPTARG};;
-		p) vcpus=${OPTARG};;
-		r) ram=${OPTARG};;
-		m) salted="1";;
-		*) usage;;
-	esac
+while getopts "h:f:t:s:p:r:i:m" OPTION; do
+  case $OPTION in
+    h) host_name=${OPTARG};;
+    f) flavor=${OPTARG};;
+    t) network=${OPTARG};;
+    s) storage=${OPTARG};;
+    p) vcpus=${OPTARG};;
+    r) ram=${OPTARG};;
+    i) ipaddr=${OPTARG};;
+    m) salted="1";;
+    *) usage;;
+  esac
 done
 
 # make sure we have necessary variables
 if [ -z "${host_name}" -o -z "${network}" ]; then
-	usage
+  usage
 fi
 
 if [ ${#} -eq 0 ]; then
-	usage
+  usage
 fi
 
 # RAM is tricky, don't let me be stupid and specify more RAM than I have. Capping out at 16.
@@ -66,22 +69,22 @@ fi
 
 # turn the flavor variable into a location for images
 case ${flavor} in
-	bionic) image="${image_dir}/standard/bionic.qcow2"; salted_image="${image_dir}/salted/bionic.qcow2"; variant="ubuntu18.04";;
-	focal) image="${image_dir}/standard/focal.qcow2"; salted_image="${image_dir}/salted/focal.qcow2"; variant="ubuntu20.04";;
-	jammy) image="${image_dir}/standard/jammy.qcow2"; salted_image="${image_dir}/salted/jammy.qcow2"; variant="ubuntu20.04";;
-	centos7) image="${image_dir}/standard/centos7.qcow2"; salted_image="${image_dir}/salted/centos7.qcow2"; variant="centos7.0";;
-	alma8) image="${image_dir}/standard/almalinux8.qcow2"; salted_image="${image_dir}/salted/almalinux8.qcow2"; variant="centos8";;
-	rocky8) image="${image_dir}/standard/rocky8.qcow2"; salted_image="${image_dir}/salted/rocky8.qcow2"; variant="centos8";;
-	buster) image="${image_dir}/standard/buster.qcow2"; salted_image="${image_dir}/salted/buster.qcow2"; variant="debian10";;
-	bullseye) image="${image_dir}/standard/bullseye.qcow2"; salted_image="${image_dir}/salted/bullseye.qcow2"; variant="debian10";;
-	*) bad_taste;;
+  bionic) image="${image_dir}/standard/bionic.qcow2"; salted_image="${image_dir}/salted/bionic.qcow2"; variant="ubuntu18.04";;
+  focal) image="${image_dir}/standard/focal.qcow2"; salted_image="${image_dir}/salted/focal.qcow2"; variant="ubuntu20.04";;
+  jammy) image="${image_dir}/standard/jammy.qcow2"; salted_image="${image_dir}/salted/jammy.qcow2"; variant="ubuntu20.04";;
+  centos7) image="${image_dir}/standard/centos7.qcow2"; salted_image="${image_dir}/salted/centos7.qcow2"; variant="centos7.0";;
+  alma8) image="${image_dir}/standard/almalinux8.qcow2"; salted_image="${image_dir}/salted/almalinux8.qcow2"; variant="centos8";;
+  rocky8) image="${image_dir}/standard/rocky8.qcow2"; salted_image="${image_dir}/salted/rocky8.qcow2"; variant="centos8";;
+  buster) image="${image_dir}/standard/buster.qcow2"; salted_image="${image_dir}/salted/buster.qcow2"; variant="debian10";;
+  bullseye) image="${image_dir}/standard/bullseye.qcow2"; salted_image="${image_dir}/salted/bullseye.qcow2"; variant="debian10";;
+  *) bad_taste;;
 esac
 
 # network needs to be validated - expected to be a valid bridge interface name
 test -L /sys/class/net/${network}
 if [ $? -ne 0 ]; then
-	echo "${network} is not a valid network interface - exiting."
-	exit 255
+  echo "${network} is not a valid network interface - exiting."
+  exit 255
 fi
 
 # variablize (is that a word?) this so I don't have to type it again in this script
@@ -90,28 +93,52 @@ ci_image=${target_dir}/${host_name}-ci.qcow2
 
 # if salted flag is passed, swap out image for salted_image
 if [ -n "${salted}" ]; then
-	image="${salted_image}"
+  image="${salted_image}"
 fi
 
 # I HAVE been known to forget to create a flavor on the VM host (with virt-sysprep), so we should... check that.
 test -f ${image}
 if [ $? -ne 0 ]; then
-	echo "Oops. Missing disk image for ${flavor}. Exiting..."
-	exit 255
+  echo "Oops. Missing disk image for ${flavor}. Exiting..."
+  exit 255
 fi
 
 # is this VM already defined on the hypervisor?
 test -f /etc/libvirt/qemu/${vmname}.xml
 if [ $? -eq 0 ]; then
-	echo "VM ${host_name} already defined -- exiting."
-	exit 255
+  echo "VM ${host_name} already defined -- exiting."
+  exit 255
 fi
 
 # does this disk image already exist? Overwriting a running domain's disk, that's bad news, so don't do it.
 test -f ${disk_image}
 if [ $? -eq 0 ]; then
-	echo "${disk_image} exists already -- exiting."
-	exit 255
+  echo "${disk_image} exists already -- exiting."
+  exit 255
+fi
+
+# does the FQDN resolve and ping?
+ping -q -c1 -w1 ${hostname} >& /dev/null
+if [ $? -eq 0 ]; then
+  echo "Error: ${hostname} is alive and responding to pings. Exiting."
+  exit 255
+fi
+
+# network validation
+# maybe I should stop asking for a NIC name above, and start referencing VLAN IDs only. something to think about.
+if [ -n "${ipaddr}" ]; then
+  vlan_id="${network: -2}"
+  ip_octet=$(echo ${ipaddr} | cut -d \. -f 3)
+  if [ ${vlan_id} -ne ${ip_octet} ]; then
+    echo "Error: IP address you specified does not match VLAN. Exiting."
+    exit 255
+  fi
+
+  ping -q -c1 -w1 $(echo ${ipaddr} | cut -d \/ -f 1) >& /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Error: ${ipaddr} is alive and responding to pings. Exiting."
+    exit 255
+  fi
 fi
 
 # create a temp dir for user-data and meta-data files
@@ -120,16 +147,16 @@ tmpdir=$(mktemp -d)
 # for virt-sysprep --copy-in to work correctly, the directory name MUST be nocloud
 mkdir ${tmpdir}/nocloud
 if [ $? -ne 0 ]; then
-	echo "Error creating ${tmpdir}/nocloud -- exiting."
-	exit 255
+  echo "Error creating ${tmpdir}/nocloud -- exiting."
+  exit 255
 fi
 
 # copy the disk image to the right location, and then resize it
 echo "Copying ${image} to ${disk_image} and resizing to ${storage}G..."
 sudo cp ${image} ${disk_image} && sudo qemu-img resize ${disk_image} ${storage}G
 if [ $? -ne 0 ]; then
-	echo "Something went wrong either with copying the image, or resizing it -- exiting."
-	exit 255
+  echo "Something went wrong either with copying the image, or resizing it -- exiting."
+  exit 255
 fi
 
 # create a meta-data file
@@ -164,19 +191,70 @@ runcmd:
     - touch /etc/cloud/cloud-init.disabled
 EOF
 
+if [ -n "${ipaddr}" ]; then
+  # we expect this IP address to be provided in x.x.x.x/xx form
+  # TODO: regex to check it
+  gw_prefix=$(echo ${ipaddr} | cut -d \. -f 1,2,3)
+  gateway="${gw_prefix}.1"
+  if [ "$(echo ${ipaddr} | cut -d \. -f 2)" == "54" ]; then
+    search="xthorsworld.lab"
+  else
+    search="xthorsworld.com"
+  fi
+
+	if [[ "${variant}" == centos* ]]; then
+	cat << EOF > ${tmpdir}/nocloud/network-config
+version: 2
+ethernets:
+  eth0:
+    match:
+      name: "eth*"
+    addresses:
+    - ${ipaddr}
+    gateway4: ${gateway}
+    nameservers:
+      search: [${search}]
+      addresses: [${gateway}]
+EOF
+	else
+  cat << EOF > ${tmpdir}/nocloud/network-config
+version: 2
+ethernets:
+  zz-all-en:
+    match:
+      name: "en*"
+    addresses:
+    - ${ipaddr}
+    gateway4: ${gateway}
+    nameservers:
+      search: [${search}]
+      addresses: [${gateway}]
+  zz-all-eth:
+    match:
+      name: "eth*"
+    addresses:
+    - ${ipaddr}
+    gateway4: ${gateway}
+    nameservers:
+      search: [${search}]
+      addresses: [${gateway}]
+EOF
+	fi
+fi
+
 # virt-sysprep and inject user-data and meta-data
 sudo virt-sysprep -a ${disk_image} --hostname ${host_name} --network --update --run-command 'mkdir -p /var/lib/cloud/seed' --copy-in ${tmpdir}/nocloud:/var/lib/cloud/seed --selinux-relabel
 if [ $? -ne 0 ]; then
-	echo "virt-sysprep exited with a non-zero status -- exiting."
-	exit 255
+  echo "virt-sysprep exited with a non-zero status -- exiting."
+  exit 255
 fi
 
 # kick off virt-install
 echo "Installing VM ${host_name}..."
 sudo virt-install --virt-type kvm --name ${host_name} --ram ${memory} --vcpus ${vcpus} \
-	--os-variant ${variant} --network=bridge=${network},model=virtio --graphics vnc \
-	--disk path=${disk_image},cache=writeback \
-	--noautoconsole --import
+  --os-variant ${variant} --network=bridge=${network},model=virtio --graphics vnc \
+  --disk path=${disk_image},cache=writeback \
+  --noautoconsole --import
 
 # cleanup
 rm -rf ${tmpdir}
